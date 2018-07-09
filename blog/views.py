b559ed1600258d0c0c32726d3f0ff86d10536d64
alaragebes <alaragb@gmail.com>
2018-07-09 12:04:43 -0700

    stuff from tutorial

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date_lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
